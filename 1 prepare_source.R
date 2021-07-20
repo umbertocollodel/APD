a79e740e57bcb56b3cef9328c142174eb42a51c1
Umberto Collodel <umberto.collodel@gmail.com>
2021-07-20 10:50:42 +0200

    script to prepare environment and load list of apd countries

---
+++ 1 prepare_source.R
@@ -0,0 +1,30 @@
+# Prepare the environment: -----
+
+
+remove(list = ls())
+
+
+packages=c("tidyverse","openxlsx","readxl",
+           "countrycode","rio")
+
+
+lapply(packages, function(x){
+  do.call("require", list(x))
+}
+)
+
+
+
+
+
+# List of APD countries: -----
+
+apd_list_countries <- read.xlsx("~/Desktop/countryinformation.xlsx", sheet = "Country data") %>% 
+  select(2, AFR_IMF, APD_IMF, EUR_IMF, MCD_IMF, WHD_IMF) %>% 
+  filter(complete.cases(`CODE_IMF`)) %>% 
+  gather("region","value",AFR_IMF:WHD_IMF) %>% 
+  filter(complete.cases(value)) %>% 
+  mutate(region = str_extract(region, "^.{3}")) %>%
+  filter(region == "APD") %>% 
+  .$`CODE_IMF`
+
