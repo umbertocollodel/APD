dac96176c58640fa5e05ea5f1141883df90bb711
Umberto Collodel <umberto.collodel@gmail.com>
2021-07-27 10:47:09 +0200

    worked on lockdowns figure with scarce success

--- 1.1 stringency_lockdowns.R
+++ 1.1 stringency_lockdowns.R
@@ -1,21 +1,80 @@
-########## Script to plot stringency index:
+# Script to produce figure lockdowns and stringency
 
 
-read_xlsx("../APD_material/raw_data/health_stringency_index.xlsx", sheet = 2, skip = 1) %>% 
-  gather("date","stringency_index",2:ncol(.)) %>% 
-  mutate(Country = case_when(Country == "China, People's Republic of" ~ "China",
-                             Country == "Hong Kong Special Administrative Region, People's Republic of China" ~ "Hong Kong",
-                             T ~ Country)) %>% 
-  mutate(country.code = countrycode(Country,"country.name","imf")) %>% 
-  filter(country.code %in% apd_list_countries) %>%
-  mutate(date = str_remove(date, ",")) %>% 
-  mutate(date = as.Date(date, tryFormats = c("%B %d %Y"))) %>%
-  mutate(month = month(date)) %>% 
-  group_by(Country, month) %>% 
-  summarise(stringency_index = mean(stringency_index, na.rm = T)) %>% 
-  filter(Country != "China") %>%
-  filter(Country == "India" | Country == "Indonesia" | Country == "Philippines" | Country == "Nepal" | Country == "Malaysia") %>% 
-  ggplot(aes(month, stringency_index, group = Country,col = Country)) +
-  geom_line(size = 2) +
+countries=c("India", "Indonesia","Philippines","Korea, Republic of", "Myanmar",
+            "Thailand", "New Zealand", "Australia", "Japan", 
+            "Vietnam","China, People's Republic of")
+
+dates_reopening <- read_xlsx("../APD_material/raw_data/health_stringency_index.xlsx", sheet = 2, skip = 1) %>% 
+  gather("date","value",`January 1, 2020`:ncol(.)) %>% 
+  mutate(date = as.character(as.Date(date, format = "%B %d, %Y"))) %>% 
+  filter(date < "2020-10-10") %>%
+  filter(Country %in% countries) %>% 
+  group_by(Country) %>% 
+  mutate(lag_value = dplyr::lag(value,1)) %>% 
+  mutate(dummy = case_when(value != lag_value ~ 1,
+                           T ~ 0),
+         max = case_when(value == max(value) ~ 1,
+                         T ~0),
+         lag_max = dplyr::lag(max, 1)) %>% 
+  filter(dummy == 1 & lag_max ==1) %>% 
+  arrange(date) %>%
+  distinct(Country, .keep_all = T) %>% 
+  split(.$Country) %>% 
+  map(~ .x %>% .$date)
+
+# Death data: -----
+
+
+deaths_august <- read_xlsx("~/Desktop/ERP matrix.xlsx", sheet = "OWID Data") %>%
+  filter(countrycode(location,"country.name","imf") %in% countrycode(countries,"country.name","imf")) %>%
+  filter(AlternativeDate == "2020M8")
+
+# Death data: -----
+
+deaths_data <- read_xlsx("~/Desktop/ERP matrix.xlsx", sheet = "new_deaths_per_million") %>% 
+  gather("country","deaths",World:ncol(.)) %>% 
+  filter(countrycode(country,"country.name","imf") %in% countrycode(countries,"country.name","imf")) %>%
+  mutate(country = case_when(country == "South Korea" ~ "Korea",
+                             T ~ country)) %>% 
+  mutate(date = as.character(date)) %>% 
+  split(.$country) 
+
+# Peak deaths before reopening: -----
+
+max_deaths <- deaths_data %>% 
+  map2(dates_reopening, ~ .x %>% filter(date <= .y)) %>% 
+  map(~ .x %>% slice(which.max(deaths))) %>% 
+  bind_rows() %>% 
+  rename(max_deaths = deaths) %>% 
+  select(country, max_deaths)
+
+
+
+# Combine information: ----
+
+
+deaths_reopening <- deaths_data %>% 
+  map2(dates_reopening, ~ .x %>% filter(date %in% .y)) %>% 
+  bind_rows() %>% 
+  rename(deaths_reopening = deaths) %>% 
+  filter(country != "Vietnam")
+  
+
+max_deaths %>% 
+  merge(deaths_reopening) %>% 
+  mutate(change = (deaths_reopening - max_deaths)/max_deaths)
+
+
+
+# Plot: ------
+
+
+
+deaths_august %>% 
+  ggplot(aes(location, NewConfirmedDeaths)) +
+  geom_point() +
   theme_minimal()
+
+  
 
