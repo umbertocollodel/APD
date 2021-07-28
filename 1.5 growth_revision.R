953364155bcfe7154c751d51683d388650ed99df
Umberto Collodel <umberto.collodel@gmail.com>
2021-07-28 10:58:14 +0200

    changed graph with table

--- 1.5 growth_revision.R
+++ 1.5 growth_revision.R
@@ -5,18 +5,11 @@ readRDS("~/Dropbox/Forecasts_Time_Covid/Forecasts_Time_Covid_material/intermedia
   filter(country_code %in% apd_list_countries) %>% 
   filter(horizon == "Jan") %>% 
   mutate(diff = -(Actual - abs(value))) %>% 
-  mutate(country = fct_reorder(country, diff, mean)) %>% 
-  ggplot(aes(diff, country)) +
-  geom_col(width = 0.5) +
-  xlab("") +
-  ylab("") +
-  theme_minimal() +
-  theme(panel.grid.major.y = element_blank()) +
-  theme(axis.text.y = element_text(size = 18),
-        axis.text.x = element_text(size = 18),
-        axis.title.y = element_text(size = 20))
-
-# Export:
-
-ggsave("../APD_material/output/figures/growth_revision.pdf")
-
+  select(country, value, Actual,diff) %>% 
+  arrange(-diff) %>% 
+  setNames(c("Country","January 2020 Projection","April 2021 Estimate","Difference from Pre-Pandemic Jan. 2020
+  World Economic Outlook (abs)")) %>% 
+  write.table(file = "../APD_material/output/tables/growth_revision.pdf",
+              row.names = F,
+              quote = F,
+              sep = ",")
