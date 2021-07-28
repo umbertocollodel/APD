3aae4c60a6ad75e50cedaa2c53cc873d6e5f8bcc
Umberto Collodel <umberto.collodel@gmail.com>
2021-07-28 11:17:16 +0200

    new changes to table

--- 1.5 growth_revision.R
+++ 1.5 growth_revision.R
@@ -9,7 +9,8 @@ readRDS("~/Dropbox/Forecasts_Time_Covid/Forecasts_Time_Covid_material/intermedia
   arrange(-diff) %>% 
   setNames(c("Country","January 2020 Projection","April 2021 Estimate","Difference from Pre-Pandemic Jan. 2020
   World Economic Outlook (abs)")) %>% 
-  write.table(file = "../APD_material/output/tables/growth_revision.pdf",
+  mutate_if(is.numeric, round, 2) %>% 
+  write.table(file = "../APD_material/output/tables/growth_revision.txt",
               row.names = F,
               quote = F,
               sep = ",")
